47e88d66ffb12d2a40ec23eba778a2bc5ba1bbed
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2025-01-21 11:59:08 +0000

    Malas isi commit message

--- packages/database/dist/client.js
+++ packages/database/dist/client.js
@@ -3,21 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var _exportNames = {
-  prisma: true
-};
 exports.prisma = void 0;
 var _client = require("@prisma/client");
-Object.keys(_client).forEach(function (key) {
-  if (key === "default" || key === "__esModule") return;
-  if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
-  if (key in exports && exports[key] === _client[key]) return;
-  Object.defineProperty(exports, key, {
-    enumerable: true,
-    get: function () {
-      return _client[key];
-    }
-  });
+// lib/prisma.ts
+
+const globalForPrisma = global;
+const prisma = exports.prisma = globalForPrisma.prisma || new _client.PrismaClient({
+  log: ['query']
 });
-const prisma = exports.prisma = global.prisma || new _client.PrismaClient();
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = global.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-
-export * from "@prisma/client";
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
