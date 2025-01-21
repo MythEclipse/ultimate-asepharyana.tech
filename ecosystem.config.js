fdb3a858096f64380d13f5d88abdfe0ed8201f6b
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2025-01-21 10:08:37 +0000

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: 'asepharyana.cloud',
+      name: 'ultimate-asepharyana.cloud',
       script: 'npm',
       args: 'start',
       env: {
         NODE_ENV: 'production',
       },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '2G',
       env_production: {
         DOTENV_CONFIG_PATH: './.env',
       },
--- pm2setup.sh
+++ pm2setup.sh
@@ -1,1 +1,2 @@
-pm2 start npm --name "asepharyana.cloud" -- start
+# pm2 start npm --name "asepharyana.cloud" -- start
+pm2 start ecosystem.config.js
