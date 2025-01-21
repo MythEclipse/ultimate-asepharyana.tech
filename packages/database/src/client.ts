c3acf5b0ebb597eeced913a959cd2928f5eb7c6b
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2025-01-21 12:30:08 +0000

    refactor: simplify Prisma client initialization and improve global variable declaration

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,16 +1,12 @@
-// lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-interface GlobalPrisma {
-  prisma?: PrismaClient;
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
 }
 
-const globalForPrisma = global as GlobalPrisma;
+export const prisma = global.prisma || new PrismaClient();
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export * from "@prisma/client";
