3ce6c92c22fa5f6a7eb6d6cc782cb3a5d0426a78
Asep Haryana <superaseph@gmail.com>
2025-01-25 14:32:13 +0800

    Malas isi commit message

--- apps/Express/src/services/websocketService.ts
+++ apps/Express/src/services/websocketService.ts
@@ -6,10 +6,10 @@ import logger from '@/utils/logger';
 export const initWebSocketServer = (server: HTTPServer) => {
   const io = new SocketIOServer(server, {
     cors: {
-      origin: "https://asepharyana.cloud",
-      methods: ["GET", "POST"],
-      credentials: true
-    }
+      origin: 'https://asepharyana.cloud',
+      methods: ['GET', 'POST'],
+      credentials: true,
+    },
   });
 
   io.on('connection', (socket) => {
