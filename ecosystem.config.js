e4ffba0aa1f571131d73bf4445a23335c389e583
Asep Haryana <superaseph@gmail.com>
2025-01-30 01:03:57 +0800

    Malas isi commit message

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
       name: 'express',
       script: 'npm',
       interpreter: 'node',
-      exec_mode: 'cluster',  // Mode cluster untuk load balancing
-      instances: 'max',      // Gunakan semua core CPU
+      exec_mode: 'fork',  // Mode cluster untuk load balancing
+      instances: '1',      // Gunakan semua core CPU
       args: 'run express',
       env: {
         NODE_ENV: 'production',
@@ -22,11 +22,11 @@ module.exports = {
       script: 'npm',
       args: 'run nextjs',
       interpreter: 'node',
-      exec_mode: 'cluster',  // Mode cluster untuk load balancing
-      instances: 'max',      // Gunakan semua core CPU
+      exec_mode: 'fork',  // Mode cluster untuk load balancing
+      instances: '1',      // Gunakan semua core CPU
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: '2G',
       env_production: {
         DOTENV_CONFIG_PATH: './.env',
       },
