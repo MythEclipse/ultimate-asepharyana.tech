8733492b9ffe8a374712630bbafbe4c4bfe32508
MythEclipse <asepharyanalm001@gmail.com>
2025-02-09 13:50:33 +0000

    refactor: update pm2 setup script to start NextJS and Express applications

--- pm2setup.sh
+++ pm2setup.sh
@@ -1,2 +1,3 @@
-pm2 start npm --name "asepharyana.cloud" -- start
+cd ./apps/NextJS/ && pm2 start npm --name "nextjs" -- start
+cd ./apps/Express/ && pm2 start npm --name "express" -- start
 # pm2 start ecosystem.config.js
