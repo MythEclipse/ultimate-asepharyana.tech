315042b0c4f1a8b00a449ef5ef90078d01eb075c
MythEclipse <asepharyanalm001@gmail.com>
2025-02-22 12:06:34 +0000

    Update rsync script to exclude .git directory during synchronization

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,1 @@
-rsync -avz --progress -e "ssh -p 22" --exclude=".turbo" /workspaces/ultimate-asepharyana.cloud/ root@217.15.165.147:/root/ultimate-asepharyana.cloud/
-# ssh -p 22 root@217.15.165.147 "pm2 restart express --update-env && pm2 restart nextjs --update-env"
+rsync -avz --progress -e "ssh -p 22" --exclude=".turbo" --exclude=".git" /workspaces/ultimate-asepharyana.cloud/ root@217.15.165.147:/root/ultimate-asepharyana.cloud/
