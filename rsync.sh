20ef6c05d6b3e091c2ba173a3f4e2fc0ff29e7ac
MythEclipse <asepharyanalm001@gmail.com>
2025-02-22 12:02:47 +0000

    Malas isi commit message

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,2 @@
 rsync -avz --progress -e "ssh -p 22" --exclude=".turbo" /workspaces/ultimate-asepharyana.cloud/ root@217.15.165.147:/root/ultimate-asepharyana.cloud/
+# ssh -p 22 root@217.15.165.147 "pm2 restart express --update-env && pm2 restart nextjs --update-env"
