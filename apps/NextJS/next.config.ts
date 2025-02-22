1d52da7b80a42c7d51093b1723939b52d9fdc7f4
MythEclipse <asepharyanalm001@gmail.com>
2025-02-22 14:54:05 +0000

    Malas isi commit message

--- apps/NextJS/next.config.ts
+++ apps/NextJS/next.config.ts
@@ -1,7 +1,7 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'standalone',
+  output: 'standalone',
   experimental: {
     optimizeCss: true,
     nextScriptWorkers: true,
--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,4 @@
 rsync -avz --progress --delete --force -e "ssh -p 22" /workspaces/ultimate-asepharyana.cloud/apps/NextJS/.next root@217.15.165.147:/root/ultimate-asepharyana.cloud/apps/NextJS/.next
 rsync -avz --progress --delete --force -e "ssh -p 22" /workspaces/ultimate-asepharyana.cloud/apps/Express/dist root@217.15.165.147:/root/ultimate-asepharyana.cloud/apps/Express/dist
+ssh -p 22 root@217.15.165.147 "/root/.local/share/pnpm/pm2 restart express --update-env && /root/.local/share/pnpm/pm2 restart nextjs --update-env"
+
