0a272f9f9b27bb97d6eb45cc7e7c6e18d8bf8ab9
MythEclipse <asepharyanalm001@gmail.com>
2025-02-22 13:30:51 +0000

    Malas isi commit message

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -avz --progress -e "ssh -p 22" --exclude=".turbo" --exclude=".git" /workspaces/ultimate-asepharyana.cloud/ root@217.15.165.147:/root/ultimate-asepharyana.cloud/
+rsync -avz --progress --delete --force -e "ssh -p 22" /workspaces/ultimate-asepharyana.cloud/apps/NextJS/.next root@217.15.165.147:/root/ultimate-asepharyana.cloud/apps/NextJS/.next
