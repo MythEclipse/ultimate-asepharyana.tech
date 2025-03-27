1477b00002d37db55b35b9e62c5b5eeb2b107a3f
MythEclipse <asepharyanalm001@gmail.com>
2025-03-27 12:00:24 +0000

    Update BaseUrl to use localhost IP address

--- apps/NextJS/lib/url.ts
+++ apps/NextJS/lib/url.ts
@@ -2,4 +2,4 @@ export const ANIMEAPI: string = 'https://otakudesu.asepharyana.cloud';
 export const KOMIK: string | undefined = process.env.NEXT_PUBLIC_KOMIK;
 export const PRODUCTION: string =
   process.env.NEXT_PUBLIC_PROD || 'https://asepharyana.cloud';
-export const BaseUrl: string = 'http://localhost:4090';
+export const BaseUrl: string = 'http://127.0.0.1:4090';
