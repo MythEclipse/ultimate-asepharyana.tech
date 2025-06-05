1965611344c41896cebc75521755ba5ec10f02f0
MythEclipse <asepharyanalm001@gmail.com>
2025-06-05 12:13:59 +0000

    Refactor: update package dependencies and improve import statements across multiple files

--- apps/NextJS/hooks/usePosts.ts
+++ apps/NextJS/hooks/usePosts.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BaseUrl } from '@/lib/url';
-import { Posts, User, Likes, Comments } from '@prisma/client';
+import { Posts } from '@asepharyana/database';
 
 export const usePosts = () => {
   const [posts, setPosts] = useState<Posts[]>([]);
