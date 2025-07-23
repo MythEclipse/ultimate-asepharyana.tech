f47c72a908cd113b821306a201c0159e9c964fac
MythEclipse <asepharyanalm001@gmail.com>
2025-07-23 17:20:22 +0700

    fix: update imports to use database package for Prisma client types

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -8,3 +8,4 @@ if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
 export { PrismaClient, Prisma };
+export type { Posts, User, Likes, Comments } from '@prisma/client';
