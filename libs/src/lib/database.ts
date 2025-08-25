09f34c80ddd37fd3ddce0ff2a6a9d8cd6eb9aad2
MythEclipse <asepharyanalm001@gmail.com>
2025-08-25 16:32:24 +0700

    a

--- libs/src/lib/database.ts
+++ libs/src/lib/database.ts
@@ -1,9 +1,9 @@
 // File: database/index.ts
-import { Prisma, PrismaClient } from './generated/client/index.js';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
 export type Posts = Prisma.PostsGetPayload<object>;
