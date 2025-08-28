9df6f7c6aba1f0810a13407a64d8bc42b6f3ec08
MythEclipse <asepharyanalm001@gmail.com>
2025-08-28 19:33:07 +0700

    feat(deploy): create target directory for Next.js and Rust builds on VPS

---
+++ storybook-ui/src/lib/storybook-ui.tsx
@@ -0,0 +1,11 @@
+import styles from './storybook-ui.module.css';
+
+export function StorybookUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to StorybookUi!</h1>
+    </div>
+  );
+}
+
+export default StorybookUi;
