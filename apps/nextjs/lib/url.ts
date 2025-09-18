cf8111c89b3d89ebc4a9948c8246a1536dac5ff8
MythEclipse <asepharyanalm001@gmail.com>
2025-09-18 18:23:18 +0700

    feat: Refactor API URL constants for improved clarity and consistency

--- apps/nextjs/lib/url.ts
+++ apps/nextjs/lib/url.ts
@@ -1,6 +1,7 @@
 export const KOMIK: string | undefined = process.env.NEXT_PUBLIC_KOMIK;
 export const PRODUCTION: string =
   process.env.NEXT_PUBLIC_PROD || 'https://asepharyana.tech';
-export const BaseUrl = 'http://localhost:4091';
-export const APIURL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4091';
+export const APIURLSERVER: string =
+  process.env.NEXT_PUBLIC_API_URL_SERVER || 'http://localhost:4091';
+export const APIURLCLIENT: string =
+  process.env.NEXT_PUBLIC_API_URL_CLIENT || 'https://ws.asepharyana.tech';
