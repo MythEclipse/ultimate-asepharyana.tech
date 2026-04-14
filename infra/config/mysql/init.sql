f1c0126d039b887e72f483ee0d75bef8f24543c4
MythEclipse <asepharyanalm001@gmail.com>
2026-04-14 21:06:39 +0700

    feat: configure MySQL exporter user and credentials for Prometheus monitoring

---
+++ infra/config/mysql/init.sql
@@ -0,0 +1,4 @@
+-- Create exporter user for Prometheus monitoring
+CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporterpassword' WITH MAX_USER_CONNECTIONS 3;
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
+FLUSH PRIVILEGES;
