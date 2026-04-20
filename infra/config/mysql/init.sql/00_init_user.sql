b76b0f74a36ee06b0aa3c40096dc1a69848c74b4
MythEclipse <asepharyanalm001@gmail.com>
2026-04-21 00:24:14 +0700

    chore: add MySQL exporter user and configure Prometheus monitoring target

--- infra/config/mysql/init.sql/00_init_user.sql
+++ infra/config/mysql/init.sql/00_init_user.sql
@@ -3,9 +3,11 @@ CREATE DATABASE IF NOT EXISTS `tracer_study`;
 
 -- Create dedicated user for tracer_study
 CREATE USER IF NOT EXISTS 'tracerstudy'@'%' IDENTIFIED BY 'tracerstudy_secret';
-
--- Grant privileges
 GRANT ALL PRIVILEGES ON `tracer_study`.* TO 'tracerstudy'@'%';
+
+-- Create dedicated user for MySQL Exporter
+CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporterpassword';
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
 
 -- Flush privileges
 FLUSH PRIVILEGES;
